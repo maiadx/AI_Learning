9dc977ada445c6cbe05068f168f2f698300c14ef
Maia Doxtader <maiadoxtader@gmail.com>
2024-10-23 12:34:17 -0500

    tutorial pt. 2

--- gpt.py
+++ gpt.py
@@ -6,8 +6,8 @@ from torch.nn import functional as Func
 # hyperparameters:
 batch_size = 64  # how many independent sequences will we process in parallel?
 block_size = 256   # what is the maximum context length for predictions?
-max_iters = 5000 # training iters
-eval_interval = 500
+max_iters = 500 # training iters
+eval_interval = 250
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
@@ -124,7 +124,7 @@ def estimate_loss():
 
 class Self_Attn_Head(nn.Module):
 
-    def __init__(self):
+    def __init__(self, head_size):
         super().__init__()
         # each token directly reads off the logits of the next token from a lookup table
         self.key = nn.Linear(n_embedding_dim, head_size, bias=False)
@@ -143,8 +143,8 @@ class Self_Attn_Head(nn.Module):
         # compute attention scores -- =affinities"
         
         wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
-        wei - wei.masked_fil(self.tril[:T, :T] == 0, float('int')) # (B, T, T)
-        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei - wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        wei = Func.softmax(wei, dim=-1) # (B, T, T)
         wei = self.dropout(wei)
         # perform the weighted aggregation of the values : 
         v = self.value(x) # (B, T, hs)
@@ -171,7 +171,7 @@ class FeedForward(nn.Module):
     """ simple linear layer followed by a non-linearity """
 
     def __init__(self, n_embedding_dim):
-        super(),__init__()
+        super().__init__()
         self.net = nn.Sequential(
             nn.Linear(n_embedding_dim, 4 * n_embedding_dim),
             nn.ReLU(),
@@ -190,7 +190,7 @@ class Block(nn.Module):
         head_size = n_embedding_dim // n_head
         self.sa = MultiheadAttention(n_head, head_size)
         self.ffwd = FeedForward(n_embedding_dim)
-        self.ln1 = nn.layerNorm(n_embedding_dim)
+        self.ln1 = nn.LayerNorm(n_embedding_dim)
         self.ln2 = nn.LayerNorm(n_embedding_dim)
 
     def forward(self, x):
@@ -213,57 +213,57 @@ class GPTLanguageModel(nn.Module):
         self.apply(self._init_weights)
         # how we initialize our weights changes how well the model is trained in its affinities.
 
-        def _init_weights(self, module):
-            if isinstance(module, nn.Linear):
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+            elif isinstance(module, nn.Embedding):
                 torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
-                if module.bias is not None:
-                    torch.nn.init.zeros_(module.bias)
-                elif isinstance(module, nn.Embedding):
-                    torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
-        def forward(self, idx, targets=None):
-            B, T = idx.shape
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
 
-            # idx and targets are booth (B, T) tensor of integers
-            tok_emb = self.token_embedding_table(idx)
-            pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
-            x = tok_emb + pos_emb # (B, T, C)
-            x = self.blocks(x) # (B,T,C)
-            x = self.ln_f(x) # (B,T,C)
-            logits = self.lm_head(x) # (B, T, vocab_size)
+        # idx and targets are booth (B, T) tensor of integers
+        tok_emb = self.token_embedding_table(idx)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
+        x = tok_emb + pos_emb # (B, T, C)
+        x = self.blocks(x) # (B,T,C)
+        x = self.ln_f(x) # (B,T,C)
+        logits = self.lm_head(x) # (B, T, vocab_size)
 
-            if targets is None: 
-                loss = None
-            else: 
-                B, T, C = logits.shape
-                logits = logits.view(B*T, C)
-                targets = targets.view(B*T)
-                loss = F.cross_entropy(logits, targets)
+        if targets is None: 
+            loss = None
+        else: 
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = Func.cross_entropy(logits, targets)
 
-            return logits, loss
+        return logits, loss
 
-        def generate(self, idx, max_new_tokens):
-            # idx is (B, T) array of inidices in the current context
-            for _ in range(max_new_tokens):
-                # crop idx to the last block_size tokens
-                idx_cond = idx[:, -block_size:]
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of inidices in the current context
+        for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
 
-                # get the predictions
-                logits, loss = self(idx_cond)
-                # focus only on the last time step
-                logits = logits[:,-1,:] # becomes (B, C)
-                # apply softmax to get probabilities
-                probs = F.softmax(logits, dim=-1) # (B, C)
-                # sample from the distribution
-                idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
-                # append sampled index to the running sequence
-                idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+            # get the predictions
+            logits, loss = self(idx_cond)
+            # focus only on the last time step
+            logits = logits[:,-1,:] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = Func.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
 
-            return idx
+        return idx
 # -----------------------------------------------------------
 
 model = GPTLanguageModel()
-m = model.to(deivce)
+m = model.to(device)
 
 # print the number of parameters in the model
 print(sum(p.numel() for p in m.parameters())/1e6, 'M paramters')
@@ -289,4 +289,4 @@ for iter in range(max_iters):
 
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
 print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
-#open('more.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
+open('llm-output.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
